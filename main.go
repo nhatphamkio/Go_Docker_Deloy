abd8c90b76abceafb88d05c2ed177153de361514
nhatpham <nhatphamkio@gmail.com>
2020-07-22 21:37:37 +0900

    docker go app

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+import(
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main(){
+	fmt.Println("Go Docker-izing")
+	http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
+		fmt.Fprintf(w,"Hello world")
+	})
+	log.Fatal(http.ListenAndServe(":8081",nil))
+}
